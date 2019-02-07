463632a884c2f9d98c7ef648720189a570f5d440
Ruth Brand <ruthbrandsoprano@gmail.com>
2019-02-06 23:35:39 -0800

    adds private method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,11 +13,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
-    @article.body = params[:article][:body]
+    @article = Article.new(article_params)
     @article.save
+    
     redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 
 end
